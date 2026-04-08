17bec921df9a871546a9a3311240ae5af0a7d33c
gibran <nekorexx@gmail.com>
2026-04-08 15:06:04 +0700

    Backtick Hotfix

--- Backend/prisma/migrations/20260407000000_rework_inherent_risk_fields/migration.sql
+++ Backend/prisma/migrations/20260407000000_rework_inherent_risk_fields/migration.sql
@@ -2,10 +2,10 @@
 -- Removes impactDescription and possibilityDescription (replaced by numeric nilai fields)
 -- Adds nilaiProbabilitasInherent, nilaiDampakInherent, nilaiProbDisplayInherent, nilaiEksposureInherent
 
-ALTER TABLE `risk_measurements`
-  DROP COLUMN `impact_description`,
-  DROP COLUMN `possibility_description`,
-  ADD COLUMN `nilai_probabilitas_inherent` INT NULL,
-  ADD COLUMN `nilai_dampak_inherent`       DOUBLE NULL,
-  ADD COLUMN `nilai_prob_display_inherent` INT NULL,
-  ADD COLUMN `nilai_eksposure_inherent`    DOUBLE NULL;
+ALTER TABLE "risk_measurements"
+  DROP COLUMN "impact_description",
+  DROP COLUMN "possibility_description",
+  ADD COLUMN "nilai_probabilitas_inherent" INTEGER NULL,
+  ADD COLUMN "nilai_dampak_inherent"       DOUBLE PRECISION NULL,
+  ADD COLUMN "nilai_prob_display_inherent" INTEGER NULL,
+  ADD COLUMN "nilai_eksposure_inherent"    DOUBLE PRECISION NULL;
